74d553e23b4d3688412e076beae3ddfc17938cf3
kennethdevpc <kennethdevpc@gmail.com>
2024-07-05 15:57:51 -0600

    eventos y estados

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
+  //index es la variable, setIndex es la funcion que modifica la variable
+  const [index, setIndex] = useState(1);
+  console.log('index', index);
+  const handleClick = (e: number) => {
+    console.log('evento', e);
+    setIndex(e);
+    console.log('evento', e);
+  };
   return (
     <div>
       <ul className="list-group">
-        {data.map((elemento) => (
-          <li key={elemento} className="list-group-item">
+        {data.map((elemento, i) => (
+          <li
+            onClick={() => handleClick(i)}
+            key={elemento}
+            className={`list-group-item ${i === index ? 'active' : ''}`}
+            value={elemento}
+          >
             {elemento}
           </li>
         ))}
