e5cf32d0ed8f6e4761f2544bc6b87183cc1c4852
kennethdevpc <kennethdevpc@gmail.com>
2024-07-05 17:09:24 -0600

    pasandole funciones  a componentes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,12 +4,19 @@ import List from './components/List';
 function App() {
   const list = ['Goku', 'vegeta', 'krilim', 'gohan', 'picoro'];
 
+  const handleSelect = (elemento: string) => {
+    console.log('Imprimiendo', elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log('Texteando', elemento);
+  };
   return (
     <div>
       <CardBody title={'titulo'} text="mi texto es " />;{/* <Card body="Hola soy body" /> */}
       <Card>
         <CardBody title={'titulo'} text="mi texto es " />
-        <List data={list} />
+        <List data={list} onSelect={handleSelect} />
+        <List data={list} onSelect={handleSelect2} />
       </Card>
     </div>
   );
--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -2,23 +2,23 @@ import { useState } from 'react';
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void; //------?: para que sea opcional
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   //index es la variable, setIndex es la funcion que modifica la variable
   const [index, setIndex] = useState(1);
   console.log('index', index);
-  const handleClick = (e: number) => {
-    console.log('evento', e);
+  const handleClick = (e: number, elemento: string) => {
     setIndex(e);
-    console.log('evento', e);
+    onSelect?.(elemento); //------Uso de la funcion y le pongo "?." para que no me de error si no se le pasa la funcion
   };
   return (
     <div>
       <ul className="list-group">
         {data.map((elemento, i) => (
           <li
-            onClick={() => handleClick(i)}
+            onClick={() => handleClick(i, elemento)}
             key={elemento}
             className={`list-group-item ${i === index ? 'active' : ''}`}
             value={elemento}
