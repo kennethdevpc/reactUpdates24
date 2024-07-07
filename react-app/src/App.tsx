aa83c56f222ad1335098aee0c6433c3e87156ecb
kennethdevpc <kennethdevpc@gmail.com>
2024-07-07 15:38:08 -0600

    agregando bottones

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,60 +4,76 @@ import Button from './components/Button';
 import List from './components/List';
 
 function App() {
-  const list = ['Goku', 'vegeta', 'krilim', 'gohan', 'picoro'];
-  const list2: string[] = [];
+  const [data, setData] = useState<string[]>(['Goku', 'vegeta', 'krilim', 'gohan', 'picoro']);
 
   const handleSelect = (elemento: string) => {
-    console.log('Imprimiendo', elemento);
+    console.log('presiono sobre ', elemento);
   };
 
-  const contenido = (e: string) =>
-    list2.length ? (
-      <>
-        <h1>hola la longitud si existe</h1>
-        <h1>hola la longitud si existe y el dato recolectado es: {e}</h1>
-        <List data={list} onSelect={handleSelect} />
-      </>
-    ) : (
-      ''
-    );
-
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const handleClick = () => {
-    setIsLoading(true);
+  const addMinion = () => {
+    setData([...data, 'Minion']);
   };
-
+  const delMinion = () => {
+    setData(data.slice(0, data.length - 1));
+  };
   return (
-    <div>
-      {123 && 'numeros '}
-      {'hay string' && ' string texto'}
-      {undefined && 'sundefined'}
-      {null && 'null'}
-      {false && 'false'}
-      {'' && 'string vacio'}
-      {0 && 'soy cero '}
-      {list2.length && 'si hay algo en la lista'}
-      {list2.length !== 0 && 'si hay algo en la lista'}
-      <CardBody title={'titulo'} text="mi texto es " />;{/* <Card body="Hola soy body" /> */}
-      <Card>
-        <CardBody title={'titulo'} text="mi texto es " />
-        <List data={list} onSelect={handleSelect} />
-        {contenido('yo soy el dato ')}
-
-        <Button isLoading={isLoading} onClick={handleClick}>
-          press here
-        </Button>
-
-        <button
-          onClick={handleClick}
-          disabled={isLoading}
-          type="button"
-          className={isLoading ? `btn btn-primary` : `btn btn-secondary`}
-        >
-          {isLoading ? 'cargando...' : 'presiona'}
-        </button>
-      </Card>
-    </div>
+    <Card>
+      <Button onClick={addMinion}>Agregar</Button>
+      <Button onClick={delMinion}>Eliminar</Button>
+      <List data={data} onSelect={handleSelect}></List>
+    </Card>
   );
+
+  //----del punto  17 para  atras----------
+  //   const list = ['Goku', 'vegeta', 'krilim', 'gohan', 'picoro'];
+  //   const list2: string[] = [];
+  //   const handleSelect = (elemento: string) => {
+  //     console.log('Imprimiendo', elemento);
+  //   };
+  //   const contenido = (e: string) =>
+  //     list2.length ? (
+  //       <>
+  //         <h1>hola la longitud si existe</h1>
+  //         <h1>hola la longitud si existe y el dato recolectado es: {e}</h1>
+  //         <List data={list} onSelect={handleSelect} />
+  //       </>
+  //     ) : (
+  //       ''
+  //     );
+  //   const [isLoading, setIsLoading] = useState<boolean>(false);
+  //   const handleClick = () => {
+  //     setIsLoading(true);
+  //   };
+  //   return (
+  //     <div>
+  //       {123 && 'numeros '}
+  //       {'hay string' && ' string texto'}
+  //       {undefined && 'sundefined'}
+  //       {null && 'null'}
+  //       {false && 'false'}
+  //       {'' && 'string vacio'}
+  //       {0 && 'soy cero '}
+  //       {list2.length && 'si hay algo en la lista'}
+  //       {list2.length !== 0 && 'si hay algo en la lista'}
+  //       <CardBody title={'titulo'} text="mi texto es " />;{/* <Card body="Hola soy body" /> */}
+  //       <Card>
+  //         <CardBody title={'titulo'} text="mi texto es " />
+  //         <List data={list} onSelect={handleSelect} />
+  //         {contenido('yo soy el dato ')}
+  //         <Button isLoading={isLoading} onClick={handleClick}>
+  //           press here
+  //         </Button>
+  //         <button
+  //           onClick={handleClick}
+  //           disabled={isLoading}
+  //           type="button"
+  //           className={isLoading ? `btn btn-primary` : `btn btn-secondary`}
+  //         >
+  //           {isLoading ? 'cargando...' : 'presiona'}
+  //         </button>
+  //       </Card>
+  //     </div>
+  //   );
+  //----del punto  17 para  atras----------
 }
 export default App;
--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 type Props = {
-  isLoading: boolean;
+  isLoading?: boolean;
   onClick: () => void;
   children: ReactNode;
 };
