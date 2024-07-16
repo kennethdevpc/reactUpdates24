66d77df83d348248c1162a1740c0150971daf6da
kennethdevpc <kennethdevpc@gmail.com>
2024-07-16 15:22:28 -0600

    formularios

--- react-form/src/components/FormReactHook.tsx
+++ react-form/src/components/FormReactHook.tsx
@@ -2,22 +2,44 @@ import { useForm } from 'react-hook-form';
 
 function FormReactHook() {
   //----usando react hook
-  const form = useForm();
-  console.log(form);
+  type Form = {
+    name: string;
+    lastname: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>();
+  console.log(errors);
+  console.log(register('lastname'));
+
+  const onsubmit = (data: Form) => console.log("informacion Obtenida desde el 'register'", data);
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit(onsubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Nombre
         </label>
-        <input name="name" type="text" id="name" className="form-control" />
+        <input
+          {...register('name', {
+            required: 'Este campo es requerido',
+            minLength: { value: 2, message: 'Mínimo 2 caracteres' },
+            maxLength: { value: 10, message: 'No más de 10 caracteres' },
+            validate: (value) => (value.length < 4 ? 'largo minimo 4' : true),
+          })}
+          type="text"
+          id="name"
+          className="form-control"
+        />
+        {errors.name && <span>{errors?.name?.message}</span>}
       </div>
       <div className="mb-3">
         <label htmlFor="lastname" className="form-label">
           Apellido
         </label>
-        <input type="text" id="lastname" name="lastname" className="form-control" />
+        <input {...register('lastname')} type="text" id="lastname" className="form-control" />
       </div>
       <button className="btn btn-primary">Enviar</button>
     </form>
