8a4bf186b83b7e182fa85097358064683b744665
kennethdevpc <kennethdevpc@gmail.com>
2024-07-20 11:01:26 -0600

    pasando el error al campo inPut,

--- first-p/src/components/ContactForm.tsx
+++ first-p/src/components/ContactForm.tsx
@@ -10,10 +10,7 @@ type Props = {};
 
 function ContactForm({}: Props) {
   const methods = useForm<contact>({ resolver: zodResolver(contactSchema) }); //---video
-  const {
-    handleSubmit,
-    formState: { errors },
-  } = methods;
+  const { handleSubmit } = methods;
   const onsubmit = (data: contact) => {
     console.log('submit');
     console.log('data', data);
@@ -23,11 +20,8 @@ function ContactForm({}: Props) {
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onsubmit)}>
         <Input name="name">Nombre</Input>
-        {errors.name?.message ?? <p>{errors?.name?.message}</p>}
         <Input name="lastname">Apellido</Input>
-        {errors.lastname?.message ?? <p>{errors?.lastname?.message}</p>}
         <Input name="email">Correo</Input>
-        {errors.email?.message ?? <p>{errors?.email?.message}</p>}
 
         <div className="form-floating">
           <select className="form-select" aria-label="Default select example">
--- first-p/src/components/Input copy.tsx
+++ first-p/src/components/Input copy.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import { FieldErrors, useFormContext } from 'react-hook-form';
+import { contact } from '../schemas/contact';
 
 type Props = {
   name: string;
   children: React.ReactNode;
+  errors: FieldErrors<contact>;
 };
 
-function Input({ name, children }: Props) {
-  const { register, formState, getFieldState } = useFormContext();
-  const { error } = getFieldState(name, formState);
-
+function Input({ name, children, errors }: Props) {
+  const { register } = useFormContext();
   return (
     <div className="mb-3">
       <label htmlFor={name} className="form-label">
         {children}
       </label>
       <input {...register(`${name}`)} type="text" id={name} className="form-control" />
-      {error?.message && <p className="text-danger">{error.message}</p>}
+      {errors?.name?.message ?? <p>{errors?.name?.message}</p>}
     </div>
   );
 }
