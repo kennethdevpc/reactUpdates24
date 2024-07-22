6a6a034839f6fac554b5028dae8923dbba285ec0
kennethdevpc <kennethdevpc@gmail.com>
2024-07-22 15:55:07 -0600

    estado de error

--- effectos/src/App.tsx
+++ effectos/src/App.tsx
@@ -10,16 +10,26 @@ type User = {
 function App() {
   const [user, setUser] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-
+  const [error, setError] = useState<string>();
   useEffect(() => {
     const url = 'https://jsonplaceholder.typicode.com/users';
     setLoading(true);
 
     fetch(url)
-      .then((response) => response.json() as Promise<User[]>) //----aqui lo que hace es convertir la data,//con un tyo de dato especifico , para que no quede any
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        return response.json() as Promise<User[]>;
+      })
+
+      //----aqui lo que hace es convertir la data,//con un tyo de dato especifico , para que no quede any
       .then((data) => {
         //-----------------este paso de pasar los datos devueltos a un tipo se llama "How to deserialize JSON object into an interface"
         setUser(data);
+      })
+      .catch((error: Error) => {
+        setError(error.message);
       })
       .finally(() => {
         setLoading(false);
@@ -28,6 +38,9 @@ function App() {
 
   if (loading) {
     return <p>Loading...</p>;
+  }
+  if (error && !loading) {
+    return <p>Ha ocurrido un Error: {error}</p>;
   }
 
   return (
