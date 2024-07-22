45df9ff91ac53f542038fdbbf2c2e915e78c68cc
kennethdevpc <kennethdevpc@gmail.com>
2024-07-22 15:31:47 -0600

    estado de cargando

--- effectos/src/App.tsx
+++ effectos/src/App.tsx
@@ -9,15 +9,26 @@ type User = {
 
 function App() {
   const [user, setUser] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     const url = 'https://jsonplaceholder.typicode.com/users';
+    setLoading(true);
+
     fetch(url)
       .then((response) => response.json() as Promise<User[]>) //----aqui lo que hace es convertir la data,//con un tyo de dato especifico , para que no quede any
       .then((data) => {
         //-----------------este paso de pasar los datos devueltos a un tipo se llama "How to deserialize JSON object into an interface"
         setUser(data);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []); //--si no se coloca nada solo se eejecuta una vez
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
