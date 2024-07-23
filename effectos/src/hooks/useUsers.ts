340585b9c7946af8d96d5b988ae1a56c2779bb46
kennethdevpc <kennethdevpc@gmail.com>
2024-07-23 12:46:13 -0600

    agregando async await en custumhook

--- effectos/src/hooks/useUsers.ts
+++ effectos/src/hooks/useUsers.ts
@@ -9,29 +9,46 @@ export default function useUsers() {
   const [user, setUser] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>();
-  useEffect(() => {
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    setLoading(true);
 
-    fetch(url)
-      .then((response) => {
+  useEffect(() => {
+    async function hook() {
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      setLoading(true);
+      try {
+        const response = await fetch(url); //---1
         if (!response.ok) {
+          //------------------2
           throw new Error(`${response.status}`);
         }
-        return response.json() as Promise<User[]>;
-      })
+        const data: User[] = await response.json(); //---3
+        setUser(data); //---4
+      } catch (error) {
+        setError((error as Error).message); //---5
+      } finally {
+        setLoading(false); //---6
+      }
 
-      //----aqui lo que hace es convertir la data,//con un tyo de dato especifico , para que no quede any
-      .then((data) => {
-        //-----------------este paso de pasar los datos devueltos a un tipo se llama "How to deserialize JSON object into an interface"
-        setUser(data);
-      })
-      .catch((error: Error) => {
-        setError(error.message);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+      // fetch(url) //----1 sin async await(SAA)
+      //   .then((response) => {
+      //     if (!response.ok) {
+      //       //--2 sin async await(SAA)
+      //       throw new Error(`${response.status}`);
+      //     }
+      //     return response.json() as Promise<User[]>; //--3-SAA
+      //   })
+      //   .then((data) => {
+      //     //----4-SAA
+      //     setUser(data);
+      //   })
+      //   .catch((error: Error) => {
+      //     setError(error.message); //--5-SAA
+      //   })
+      //   .finally(() => {
+      //     setLoading(false);//---6-SAA
+      //   });
+    }
+
+    hook();
   }, []); //--si no se coloca nada solo se eejecuta una vez
   return { user, loading, error };
 }
