3c877a150ce52ebe2a8f51fba84396967f668047
kennethdevpc <kennethdevpc@gmail.com>
2024-07-30 14:53:07 -0600

    agregando grilla

--- meal-finder/src/App.tsx
+++ meal-finder/src/App.tsx
@@ -1,42 +1,30 @@
 import { useState } from 'react';
-import { Image, Stack, Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react';
 import './App.css';
-import { Card, CardBody, CardFooter } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import Header from './components/Header';
+import SideNav from './components/SideNav';
+import MainContent from './components/MainContent';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Card maxW="sm">
-      <CardBody>
-        <Image
-          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-        />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">Living room Sofa</Heading>
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned
-            spaces and for people who love a chic design with a sprinkle of vintage design.
-          </Text>
-          <Text color="blue.600" fontSize="2xl">
-            $450
-          </Text>
-        </Stack>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Buy now
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
-          </Button>
-        </ButtonGroup>
-      </CardFooter>
-    </Card>
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"`}
+      gridTemplateRows={'60px 1fr '}
+      gridTemplateColumns={{ sm: `0 1fr`, md: `250px 1fr` }}
+    >
+      <GridItem pl="2" bg="orange.300" area={'header'}>
+        <Header></Header>
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={'nav'} height="calc(100vh - 60px)">
+        <SideNav></SideNav>
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={'main'}>
+        <MainContent></MainContent>
+      </GridItem>
+    </Grid>
   );
 }
 
